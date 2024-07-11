35b83def2fed73f739f435085673f01da15ee76e
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-07-11 20:16:47 +0800

    Create 线性筛.cpp

---
+++ 数学/线性筛.cpp
@@ -0,0 +1,43 @@
+int mu[N];int phi[N];
+int np[N];vector<int> p;
+void Prework(int n = 2e5) {//线性筛积性函数
+    np[0] = np[1] = 1;mu[1] = 1;phi[1] = 1;
+    for (int i = 2;i <= n;i++) {
+        if (!np[i]) {
+            p.push_back(i);
+            mu[i] = -1;phi[i] = i - 1;
+        }
+        for (auto j : p) {
+            if (i * j > n) break;
+            np[i * j] = 1;
+            if (i % j == 0) {
+                phi[i * j] = phi[i] * j;
+                break;
+            }
+            else {
+                mu[i * j] = -mu[i];
+                phi[i * j] = phi[i] * (j - 1);
+            }
+        }
+    }
+}
+
+
+
+int np[N], minp[N];
+vector<int> p;
+void Prework(int n = 2e5) {//欧拉筛最小质因子
+    np[0] = np[1] = 1;
+    for (int i = 2;i <= n;i++) {
+        if (!np[i]) {
+            p.push_back(i);
+            minp[i] = i;
+        }
+        for (auto j : p) {
+            if (i * j > n) break;
+            np[i * j] = 1;
+            minp[i * j] = j;
+            if (i % j == 0) break;
+        }
+    }
+}

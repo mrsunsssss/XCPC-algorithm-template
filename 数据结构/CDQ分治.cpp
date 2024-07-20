a510b32fc88c85ba168a8879a2cb4264224edcc7
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-07-20 18:27:32 +0800

    Update CDQ分治.cpp

--- 数据结构/CDQ分治.cpp
+++ 数据结构/CDQ分治.cpp
@@ -1,3 +1,4 @@
+//cdq+树状数组
 struct BIT {
     int n;
     vector<int> tr;
@@ -74,3 +75,88 @@ namespace CDQ {
         return a;
     }
 }
+
+
+
+//cdq套cdq
+namespace CDQ_CDQ {
+    struct info {
+        int a, b, c;
+        int id, res;
+        int cnt;
+        int mark;
+        bool friend operator <(const info& u, const info& v) {
+            if (u.a != v.a) return u.a < v.a;
+            if (u.b != v.b) return u.b < v.b;
+            return u.c < v.c;
+        }
+        bool friend operator == (const info& u, const info& v) {
+            return u.a == v.a && u.b == v.b && u.c == v.c;
+        }
+    };
+    bool cmp(const info& u, const info& v) {
+        return u.b < v.b;
+    }
+    const int MAXN = 2e5;
+    BIT tr(MAXN);
+    auto run(vector<array<int, 3>> b) {
+        int n = b.size();
+        vector<info> a(n);
+        for (int i = 0;i < n;i++) {
+            a[i].a = b[i][0], a[i].b = b[i][1], a[i].c = b[i][2];
+            a[i].id = i, a[i].res = 0, a[i].cnt = 1;
+        }
+        auto CDQ2 = [&](auto&& CDQ2, int l, int r) {
+            if (l == r) return;
+            vector<info> tmp;
+            int mid = l + r >> 1;
+            CDQ2(CDQ2, l, mid);CDQ2(CDQ2, mid + 1, r);
+            int i, j, ans = 0;
+            for (i = l, j = mid + 1;j <= r;j++) {
+                while (i <= mid && a[i].c <= a[j].c) {
+                    ans += a[i].cnt * (a[i].mark == 1);
+                    tmp.push_back(a[i]);
+                    i++;
+                }
+                a[j].res += ans * (a[j].mark == 2);
+                tmp.push_back(a[j]);
+            }
+            for (;i <= mid;i++) tmp.push_back(a[i]);
+            for (i = l;i <= r;i++) a[i] = tmp[i - l];
+            };
+        auto CDQ = [&](auto&& CDQ, int l, int r) {
+            if (l == r) return;
+            int mid = l + r >> 1;
+            CDQ(CDQ, l, mid);CDQ(CDQ, mid + 1, r);
+            for (int i = l;i <= mid;i++) {
+                a[i].mark = 1;
+            }
+            for (int i = mid + 1;i <= r;i++) {
+                a[i].mark = 2;
+            }
+            stable_sort(a.begin() + l, a.begin() + r + 1, cmp);
+            CDQ2(CDQ2, l, r);
+            };
+        sort(a.begin(), a.end());
+        int tot = 0;
+        for (int i = 1;i < n;i++) {
+            if (a[i] == a[tot]) a[tot].cnt += 1;
+            else a[++tot] = a[i];
+        }
+        a.resize(tot + 1);
+        CDQ(CDQ, 0, tot);
+        return a;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+

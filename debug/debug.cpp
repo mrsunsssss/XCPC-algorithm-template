6fe9cd08c501ca3a14fe1ebbb24b41d9e69ccd20
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-07-21 12:27:51 +0800

    Update debug.cpp

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -1,3 +1,9 @@
 template <class... Args>
 void debug(const Args&... args) { ((cerr << args << ", "), ...) << '\n'; }
 #define debug(args...) cerr << #args << ": ", debug(args)
+
+
+template <class... Args> void myin(Args&... args) { ((cin >> args), ...); }
+template <class... Args> void myout(const Args&... args) { ((cout << args), ...); }
+//ofstream mcout("C:/Users/Mrsuns/Desktop/out.txt");
+//ofstream mcout("C:/Users/Mrsuns/Desktop/out.txt");

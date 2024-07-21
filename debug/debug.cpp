60f993af7d91c98bedb290d3530ddba1b857356e
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-07-21 12:42:53 +0800

    Update debug.cpp

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -1,6 +1,13 @@
-template <class... Args>
-void debug(const Args&... args) { ((cerr << args << ", "), ...) << '\n'; }
+#ifdef LOCAL
+template <class... Args> void debug(const Args&... args) { ((cerr << args << ", "), ...) << '\n'; }
 #define debug(args...) cerr << #args << ": ", debug(args)
+#define debugsq(x) cerr << #x << ": ["; for (auto i : x) cerr << i << ' '; cerr << "]\n";
+#define debugmp(x) cerr << #x << ": [ "; for (auto [i, j] : x) cerr << '[' << i << "," << j << "] "; cerr << "]\n";
+#else
+#define debug(...) ;
+#define debugsq(...) ;
+#define debugmp(...) ;
+#endif
 
 
 template <class... Args> void myin(Args&... args) { ((cin >> args), ...); }

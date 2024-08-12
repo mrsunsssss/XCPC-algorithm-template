f4d976b5c24abc1c72a6557a75968dcb1dd3ac2c
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-08-12 23:27:22 +0800

    Create 矩阵类.cpp

---
+++ 数学/矩阵/矩阵类.cpp
@@ -0,0 +1,53 @@
+const int maxn = 210;
+const int MOD = 1e9 + 7;
+struct matrix {
+    int n;
+    int data[maxn][maxn];
+    matrix() = default;
+    matrix(int n) :n(n) {}
+    int* operator[] (int idx) { return data[idx]; }
+    void dbg() {
+        cout << "-----matrix:begin-----\n";
+        for (int i = 1;i <= n;i++) {
+            for (int j = 1;j <= n;j++) {
+                cout << data[i][j] << ' ';
+            }
+            cout << endl;
+        }
+        cout << "-----matrix:end-----\n";
+    }
+};
+matrix operator*(matrix& A, matrix& B) {
+    int n = A.n;
+    matrix C(n);
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            for (int k = 1; k <= n; k++) {
+                C[i][j] += A[i][k] * B[k][j] % MOD;
+                if (C[i][j] >= MOD) C[i][j] -= MOD;
+            }
+
+    return C;
+}
+matrix operator+(matrix& A, matrix& B) {
+    int n = A.n;
+    matrix C(n);
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            for (int k = 1; k <= n; k++) {
+                C[i][j] = A[i][k] * B[k][j];
+                if (C[i][j] >= MOD) C[i][j] -= MOD;
+            }
+
+    return C;
+}
+matrix qp(matrix A, int t) {
+    int n = A.n;
+    matrix ans(n);
+    for (int i = 1; i <= n; i++) ans[i][i] = 1;
+    while (t) {
+        if (t & 1) ans = ans * A;
+        t >>= 1;A = A * A;
+    }
+    return ans;
+}

6d30892fbc7e5a9ccc38b7a869decfca71ef8581
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-09-14 22:08:10 +0800

    Update 线段树.cpp

--- 数据结构/线段树/线段树.cpp
+++ 数据结构/线段树/线段树.cpp
@@ -174,6 +174,9 @@ private:
     }
 };
 //to use it: lazy_segtree<S,op,e,F,mapping,composition,id> tr(vector<S>/int);
+
+//op：区间乘，区间加。
+//ask：区间和
 struct S {
     int sum, len;
 };
@@ -195,3 +198,37 @@ F composition(F f, F g) { // f(g(x))
 F id() {//e of F
     return F{ 1,0 };
 }
+
+
+//op：区间赋值
+//ask：区间和
+struct S {
+    int sum, len;
+};
+S op(S l, S r) {
+    return S{ l.sum + r.sum,l.len + r.len };
+}
+S e() {
+    return S{ 0,1 };
+}
+struct F {
+    int add;
+};
+S mapping(F f, S x) {//x->f(x)
+    if (f.add == inf) return S{ x.sum,x.len };
+    else {
+        return S{ f.add * x.len,x.len };
+    }
+}
+F composition(F f, F g) { // f(g(x))
+    if (f.add == inf) return F{ g.add };
+    return F{ f.add };
+}
+F id() {//e of F
+    return F{ inf };
+}
+
+
+
+
+

e4bf00c286a92aa3d4e79b8fa85c94297d805718
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-10-08 00:18:50 +0800

    Create 倍增LCA.cpp

---
+++ 图论/LCA/倍增LCA.cpp
@@ -0,0 +1,39 @@
+struct LCA {
+    int n;
+    vector<vector<int>> dp;
+    vector<int> dep;
+    LCA(int _n) :n(_n) {
+        dp.assign(21, vector<int>(n + 1));
+        dep.assign(n + 1, 0);
+    }
+    void dfs(vector<vector<int>>& g, int u, int p) {
+        dep[u] = dep[p] + 1;
+        for (auto v : g[u]) {
+            if (v == p) continue;
+            dp[0][v] = u;
+            dfs(g, v, u);
+        }
+    }
+    void init() {
+        for (int i = 1;i <= 20;i++) {
+            for (int j = 1;j <= n;j++) {
+                dp[i][j] = dp[i - 1][dp[i - 1][j]];
+            }
+        }
+    }
+    int query(int u, int v) {
+        if (dep[u] < dep[v]) swap(u, v);
+        for (int i = 20;i >= 0;i--) {
+            if (dep[dp[i][u]] >= dep[v]) {
+                u = dp[i][u];
+            }
+        }
+        if (u == v) return v;
+        for (int i = 20;i >= 0;i--) {
+            if (dp[i][u] != dp[i][v]) {
+                u = dp[i][u], v = dp[i][v];
+            }
+        }
+        return dp[0][u];
+    }
+};

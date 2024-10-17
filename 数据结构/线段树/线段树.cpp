c8db8a452e6960844bd71031aaae2b29c96b018c
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-10-18 01:18:03 +0800

    Update 线段树.cpp

--- 数据结构/线段树/线段树.cpp
+++ 数据结构/线段树/线段树.cpp
@@ -178,35 +178,6 @@ private:
 
 //op：区间赋值
 //ask：区间和
-struct S {
-    int sum, len;
-};
-S op(S l, S r) {
-    return S{ l.sum + r.sum,l.len + r.len };
-}
-S e() {
-    return S{ 0,1 };
-}
-struct F {
-    int add;
-};
-S mapping(F f, S x) {//x->f(x)
-    if (f.add == inf) return S{ x.sum,x.len };
-    else {
-        return S{ f.add * x.len,x.len };
-    }
-}
-F composition(F f, F g) { // f(g(x))
-    if (f.add == inf) return F{ g.add };
-    return F{ f.add };
-}
-F id() {//e of F
-    return F{ inf };
-}
-
-
-//op：区间赋值
-//ask：区间和
 //extra：线段树二分第一个大于/小于某值的位置
 struct S {
     int sum, Max, Min, len;

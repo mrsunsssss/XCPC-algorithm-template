2be4907a112234c2968a094269df382c574b892d
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-10-18 00:07:05 +0800

    Update 组合数.cpp

--- 数学/组合数.cpp
+++ 数学/组合数.cpp
@@ -2,8 +2,8 @@ int fact[N], infact[N];
 int qp(int a, int n) {
     int res = 1;
     while (n) {
-        if (n & 1) res = res * a % MOD;
-        n >>= 1, a = a * a % MOD;
+        if (n & 1) res = 1LL * res * a % MOD;
+        n >>= 1, a = 1LL * a * a % MOD;
     }
     return res;
 }
@@ -12,12 +12,12 @@ int inv(int x) {
 }
 int comb(int a, int b) {
     if (a < 0 || b < 0 || a < b) return 0;
-    return fact[a] * infact[b] % MOD * infact[a - b] % MOD;
+    return 1LL * fact[a] * infact[b] % MOD * infact[a - b] % MOD;
 }
 int __comb(int a, int b) {
     if (a < 0 || b < 0 || a < b) return 0;
     int res = infact[b];
-    for (int i = 0;i < b;i++) res = res * (a - i) % MOD;
+    for (int i = 0;i < b;i++) res = 1LL * res * (a - i) % MOD;
     return res;
 }
 int norm(int x) {
@@ -26,10 +26,10 @@ int norm(int x) {
 void Prework(int n = N - 10) {
     fact[0] = 1;
     for (int i = 1;i <= n;i++) {
-        fact[i] = fact[i - 1] * i % MOD;
+        fact[i] = 1LL * fact[i - 1] * i % MOD;
     }
     infact[n] = qp(fact[n], MOD - 2);
     for (int i = n;i >= 1;i--) {
-        infact[i - 1] = infact[i] * i % MOD;
+        infact[i - 1] = 1LL * infact[i] * i % MOD;
     }
 }

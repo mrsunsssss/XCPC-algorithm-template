18deaab18ffad41cf24f02e5a43ce1d2c84978d3
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-10-19 23:33:40 +0800

    Update ST表.cpp

--- 数据结构/ST表.cpp
+++ 数据结构/ST表.cpp
@@ -2,23 +2,23 @@ template<class T> struct StaticTable {
     int n;
     vector<T> a;
     vector<array<T, 31>> dp;
-    function<T(T, T)> F;
+    function<T(T, T)> func;
     StaticTable() {};
-    StaticTable(vector<T> a, function<T(T, T)> f) {
-        run(a, f);
+    StaticTable(vector<T> a, function<T(T, T)> F) {
+        run(a, F);
     }
     void run(vector<T>& a) {
         run(a, [&](T x, T y) { return max(x, y); });
     }
-    void run(vector<T>& a, function<T(T, T)> f) {
+    void run(vector<T>& a, function<T(T, T)> F) {
         this->a = a;
-        this->f = f;
+        this->func = F;
         this->n = (int)a.size() - 1;
         this->dp.assign(n + 1, {});
-        for (int i = 1; i <= n; i++) f[i][0] = a[i];
+        for (int i = 1; i <= n; i++) dp[i][0] = a[i];
         for (int j = 1; j <= 30; j++) {
             for (int i = 1; i + (1 << j) - 1 <= n; i++) {
-                f[i][j] = func(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+                dp[i][j] = func(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
             }
         }
     }

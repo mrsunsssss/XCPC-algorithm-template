557c5b410adc0eee48ed6b48573d4a1e666a5495
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2024-11-14 19:15:02 +0800

    Create 新版计算几何整合版.cpp

---
+++ 计算几何/新版计算几何整合版.cpp
@@ -0,0 +1,288 @@
+#define debugP(x) cerr<<#x<<":("<<x[0]<<','<<x[1]<<")"<<endl
+using PI = array<int, 2>;
+using VI = array<int, 2>;
+using LI = array<PI, 2>;
+using CI = vector<PI>;
+
+using ld = long double;
+using PD = array<ld, 2>;
+using VD = array<ld, 2>;
+using LD = array<PD, 2>;
+using CD = vector<PD>;
+const ld eps = 1e-9;
+
+using ty_g = int;
+using P = PI;
+using V = VI;
+using L = LI;
+using C = CI;
+
+namespace Tools {
+    int prev(int i, int n) { return i == 0 ? n - 1 : i - 1; }
+    int next(int i, int n) { return i == n - 1 ? 0 : i + 1; }
+
+    //int
+    int sgn(int x) { return x > 0 ? 1 : (x < 0 ? -1 : 0); }
+    VI uv(PI u, PI v) { return { v[0] - u[0],v[1] - u[1] }; }
+    VI add(VI u, VI v) { return { u[0] + v[0],u[1] + v[1] }; }
+    int dis2(PI u, PI v) { return (u[0] - v[0]) * (u[0] - v[0]) + (u[1] - v[1]) * (u[1] - v[1]); }
+    int cross(VI u, VI v) { return u[0] * v[1] - u[1] * v[0]; }//角度，顺逆，sin，三角形面积
+    int dot(VI u, VI v) { return u[0] * v[0] + u[1] * v[1]; }//投影，cos
+    int loca(PI u, PI v, PI w) { return sgn(cross(uv(u, v), uv(u, w))); }
+    int locb(PI u, PI v, PI w) { return sgn(dot(uv(u, v), uv(u, w))); }
+    VI rotate_90(VI l) { return { -l[1], l[0] }; }
+
+    //double
+    ld sgn(ld x) { return x > eps ? 1 : (x < -eps ? -1 : 0); }
+    VD uv(PD u, PD v) { return { v[0] - u[0],v[1] - u[1] }; }
+    ld cross(PD u, PD v) { return u[0] * v[1] - u[1] * v[0]; }
+    ld dot(PD u, PD v) { return u[0] * v[0] + u[1] * v[1]; }
+    ld dis(PD u, PD v) { return sqrt((u[0] - v[0]) * (u[0] - v[0]) + (u[1] - v[1]) * (u[1] - v[1])); }
+    ld dis2(PD u, PD v) { return ((u[0] - v[0]) * (u[0] - v[0]) + (u[1] - v[1]) * (u[1] - v[1])); }
+    VD mul(VD p, ld x) { return { p[0] * x,p[1] * x }; }
+    VD add(VD u, VD v) { return { u[0] + v[0],u[1] + v[1] }; }
+    VD sub(VD u, VD v) { return { u[0] - v[0],u[1] - v[1] }; }
+    VD div(VD p, ld x) { return { p[0] / x,p[1] / x }; }
+    int loca(PD u, PD v, PD w) { return sgn(cross(uv(u, v), uv(u, w))); }
+    PD project(LD l, PD p) {
+        VD base = uv(l[0], l[1]);//两点式描述直线
+        ld r = dot(uv(l[0], p), base) / (base[0] * base[0] + base[1] * base[1]);
+        return add(l[0], mul(base, r));
+    }
+    VD rotate(VD l, ld angle) {//逆时针旋转angle
+        ld cosa = cos(angle), sina = sin(angle);
+        return { l[0] * cosa - l[1] * sina, l[0] * sina + l[1] * cosa };
+    }
+    VD rotate_90(VD l) { return { -l[1], l[0] }; }
+}
+
+namespace Segments {
+    using namespace Tools;
+    bool point_on_segment(P a, L line) {
+        return sgn(cross(uv(a, line[0]), uv(a, line[1]))) == 0
+            && sgn(dot(uv(a, line[0]), uv(a, line[1]))) <= 0;
+    }
+    bool inter_judge_segment(L a, L b) {
+        //一个点的端点在另一个线段
+        if (point_on_segment(b[0], a)
+            || point_on_segment(b[1], a)
+            || point_on_segment(a[0], b)
+            || point_on_segment(a[1], b))
+            return 1;
+        //跨立试验
+        return (loca(a[0], b[0], a[1]) * loca(a[0], b[1], a[1]) < 0
+            && loca(b[0], a[0], b[1]) * loca(b[0], a[1], b[1]) < 0
+            );
+    }
+    bool inter_judge(L a, L b) {
+        return sgn(cross(uv(a[0], a[1]), uv(a[0], b[0]))
+            - cross(uv(a[0], a[1]), uv(a[0], b[1]))) != 0;
+    }
+}
+
+namespace Lines {
+    using namespace Tools;
+    PD line_inter(LD a, LD b) {
+        ld s1 = cross(uv(a[0], a[1]), uv(a[0], b[0]));
+        ld s2 = cross(uv(a[0], a[1]), uv(a[0], b[1]));
+        return div(sub(mul(b[0], s2), mul(b[1], s1)), s2 - s1);
+    }
+}
+
+namespace Circles {
+    using namespace Tools;
+    array<PD, 2> inter_circle_line(PD o, ld r, LD line) {
+        PD s = line[0], t = line[1];
+        PD pr = project(line, o);
+        ld d = dis(o, pr);
+        if (sgn(d - r) > 0) return { nan(""),nan("") };//use isnan(x) to check
+        ld len = sqrt(r * r - d * d);
+        PD dir = div(uv(s, t), dis(s, t));
+        PD inter1 = add(pr, mul(dir, len));
+        PD inter2 = add(pr, mul(dir, -len));
+        return { inter1,inter2 };
+    }
+}
+
+namespace Convex_Hull {
+    using namespace Tools;using namespace Segments;
+    vector<P> convex_hull(vector<P> a) {//Andrew求凸包(扫描线)
+        if (a.size() <= 2) return a;
+        sort(a.begin(), a.end());
+        vector<P> ret;
+        for (int i = 0;i < a.size();i++) {
+            while (ret.size() >= 2 && loca(ret[ret.size() - 2], ret[ret.size() - 1], a[i]) <= 0)
+                ret.pop_back();
+            ret.push_back(a[i]);
+        }
+        int fixed = ret.size();
+        for (int i = (int)a.size() - 2;i >= 0;i--) {
+            while (ret.size() > fixed && loca(ret[ret.size() - 2], ret[ret.size() - 1], a[i]) <= 0)
+                ret.pop_back();
+            ret.push_back(a[i]);
+        }
+        ret.pop_back();
+        return ret;
+    }
+    array<int, 2> in_convex(P p, const C& a) {//{no | strictly yes | yes,where}
+        int n = a.size();
+        if (n == 1) {
+            return { sgn(p[0] - a[0][0]) == 0 && sgn(p[1] - a[0][0]) == 0,0 };
+        }
+        if (n == 2) {
+            return { point_on_segment(p, { a[0],a[1] }),0 };
+        }
+        int l = 1, r = n - 2;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            int u = loca(a[0], a[mid], p);
+            int v = loca(a[0], a[mid + 1], p);
+            if (u >= 0 && v <= 0) {
+                if (loca(a[mid], a[mid + 1], p) >= 0) {
+                    {//在凸包的边上
+                        if (loca(a[mid], a[mid + 1], p) == 0) return { 2,mid };
+                        if (mid == 1 && loca(a[mid], a[0], p) == 0) return { 2,0 };
+                        if (mid + 1 == n - 1 && loca(a[mid + 1], a[0], p) == 0) return { 2,n - 1 };
+                    }
+                    return { 1,mid };
+                }
+                return { 0,0 };
+            }
+            if (u < 0) r = mid - 1;
+            else l = mid + 1;
+        }
+        return { 0,0 };
+    }
+
+    ty_g diam2(const C& a) {//直径平方
+        ty_g r = 0;
+        int n = a.size();
+        if (n <= 2) {
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    r = max(r, dis2(a[i], a[j]));
+            return r;
+        }
+        for (int i = 0, j = 1; i < n; i++) {
+            while (
+                cross(uv(a[i], a[next(i, n)]), uv(a[i], a[j]))
+                - cross(uv(a[i], a[next(i, n)]), uv(a[i], a[next(j, n)])) <= 0
+                ) j = next(j, n);
+            r = max({ r, dis2(a[i], a[j]), dis2(a[next(i, n)], a[j]) });
+        }
+        return r;
+    }
+    ty_g area2(const vector<P>& a) {
+        ty_g ret = 0;
+        for (int i = 0;i < a.size();i++) {
+            int j = (i + 1) % a.size();
+            ret += cross(a[i], a[j]);
+        }
+        return abs(ret);
+    }
+
+    P find_max(const C& a, auto cmp) {//极点
+        int l = 1, r = (int)a.size() - 2;
+        if (cmp(a.back(), a[0])) {
+            while (l <= r) {
+                int mid = l + r >> 1;
+                if (cmp(a[l - 1], a[mid]) && cmp(a[mid - 1], a[mid])) l = mid + 1;
+                else r = mid - 1;
+            }
+            return a[r];
+        }
+        else {
+            while (l <= r) {
+                int mid = l + r >> 1;
+                if (cmp(a[r + 1], a[mid]) && cmp(a[mid + 1], a[mid])) r = mid - 1;
+                else l = mid + 1;
+            }
+            return a[l];
+        }
+    }
+
+    //The order of the answer is counterclockwise of the convex hull.
+    array<P, 2> tangent(const C& a, P u) {//过u的切线
+        //如果点在凸包的点和边需要特判
+        return { find_max(a,[&](auto x,auto y) {return loca(u,x,y) > 0;}),
+                find_max(a,[&](auto x,auto y) {return loca(u,y,x) > 0;}) };
+    }
+
+    array<P, 2> tangent_vec(const C& a, P u) {//与u平行的切线
+        //如果点在凸包的点和边需要特判
+        return { find_max(a,[&](auto x,auto y) {return sgn(cross(u,uv(x,y))) > 0;}),
+                find_max(a,[&](auto x,auto y) {return sgn(cross(u,uv(y,x))) > 0;}) };
+    }
+
+
+    C minkovski(vector<C> A) {//结果是非严格凸包，即存在三点共线(可以通过再跑一遍凸包算法变成严格凸包)
+        auto run = [&](array<C, 2> a) {
+            for (int i = 0;i < 2;i++) a[i].push_back(a[i].front());
+            int i[2] = { 0,0 }, j[2] = { 0,0 }, len[2] = { (int)a[0].size() - 1,(int)a[1].size() - 1 };
+            vector<P> ret;
+            ret.push_back(add(a[0][0], a[1][0]));
+            do {
+                int d;
+                if (!j[0] && !j[1]) {
+                    d = sgn(cross(uv(a[1][i[1]], a[1][i[1] + 1]),
+                        uv(a[0][i[0]], a[0][i[0] + 1]))) >= 0;
+                }
+                else if (!j[0]) d = 0;
+                else if (!j[1]) d = 1;
+                else break;
+                ret.push_back(add(uv(a[d][i[d]], a[d][i[d] + 1]), ret.back()));
+                i[d] = (i[d] + 1) % len[d];
+                if (i[d] == 0) j[d] = 1;
+            } while (!j[0] || !j[1]);
+            return ret;
+            };
+        for (auto& i : A) i = convex_hull(i);
+        array<C, 2> ret;ret[0] = A[0];
+        for (int i = 1;i < A.size();i++) {
+            ret[1] = A[i];
+            ret[0] = run(ret);
+        }
+        return ret[0];
+    }
+}
+
+namespace Polygon {
+    using namespace Tools;
+
+    array<int, 2> cover(const vector<P>& a, const P& o) {//回转数法判断点是否在多边形内(O(n)),要求顺序是顺时针or逆时针
+        int cnt = 0, n = a.size();//回转数=0表示在多边形外
+        for (int i = 0; i < n; i++) {
+            P u = a[i], v = a[next(i, n)];
+            if (sgn(cross(uv(o, u), uv(o, v))) == 0 && sgn(dot(uv(o, u), uv(o, v))) <= 0) return { 1,1 };//在多边形上
+            if (sgn(u[1] - v[1]) == 0) continue;
+            if (sgn(u[1] - v[1]) < 0 && loca(u, v, o) <= 0) continue;
+            if (sgn(u[1] - v[1]) > 0 && loca(u, v, o) >= 0) continue;
+            if (sgn(u[1] - o[1]) < 0 && sgn(v[1] - o[1]) >= 0) cnt++;
+            if (sgn(u[1] - o[1]) >= 0 && sgn(v[1] - o[1]) < 0) cnt--;
+        }
+        return { cnt,0 };//返回值表示回转数及是否在多边形一条边上
+    }
+}
+
+namespace other {
+    using namespace Tools;
+    struct argcmp {//极角排序
+        inline static int DS[4] = { 1,2,4,3 };
+        bool operator()(const P& a, const P& b)const {
+            const auto quad = [&](const P& u) {
+                return DS[(sgn(u[1]) < 0) * 2 + (sgn(u[0]) < 0)];
+                };
+            int c = quad(a), d = quad(b);
+            if (c != d) return c < d;
+            return sgn(cross(a, b)) > 0;
+        }
+    };
+}
+
+using namespace Tools;
+using namespace Segments;
+using namespace Lines;
+using namespace Circles;
+using namespace Convex_Hull;
+using namespace Polygon;
+using namespace other;

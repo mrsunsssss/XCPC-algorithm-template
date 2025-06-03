4f7938ed5b890acba2c176e08ba0c9226582242e
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-06-03 14:40:44 +0800

    Update int128输入输出.cpp

--- 杂项/int128输入输出.cpp
+++ 杂项/int128输入输出.cpp
@@ -1,11 +1,11 @@
 istream& operator>>(istream& cin, __int128& a) {
-    a = 0;
-    string scan;
-    cin >> scan;
-    for (int i = 0; i < scan.size(); i++) {
-        a *= 10;
-        a += scan[i] - '0';
+    a = 0;__int128 f = 1;
+    string s;cin >> s;
+    for (auto i : s) {
+        if (i == '-') { f = -1; continue; }
+        a = a * 10 + (i - '0');
     }
+    a *= f;
     return cin;
 }
 ostream& operator<<(ostream& cout, __int128 a) {
@@ -13,8 +13,7 @@ ostream& operator<<(ostream& cout, __int128 a) {
         cout << "-";
         a = -a;
     }
-    if (a > 9)
-        cout << a / 10;
+    if (a > 9) cout << a / 10;
     cout << (char)(a % 10 + '0');
     return cout;
 }

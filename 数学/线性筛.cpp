279afb36a973359e5b37ee5e0c6c011c48dd051a
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-06-22 14:45:44 +0800

    Update 线性筛.cpp

--- 数学/线性筛.cpp
+++ 数学/线性筛.cpp
@@ -1,28 +1,3 @@
-int mu[N];int phi[N];
-int np[N];vector<int> p;
-void Prework(int n = N - 10) {//线性筛积性函数
-    np[0] = np[1] = 1;mu[1] = 1;phi[1] = 1;
-    for (int i = 2;i <= n;i++) {
-        if (!np[i]) {
-            p.push_back(i);
-            mu[i] = -1;phi[i] = i - 1;
-        }
-        for (auto j : p) {
-            if (i * j > n) break;
-            np[i * j] = 1;
-            if (i % j == 0) {
-                phi[i * j] = phi[i] * j;
-                break;
-            }
-            else {
-                mu[i * j] = -mu[i];
-                phi[i * j] = phi[i] * (j - 1);
-            }
-        }
-    }
-}
-
-
 const int N = 1e5 + 10;
 int np[N], minp[N];
 vector<int> p;

d31c73f4c0c804088d8363ba3dfbc3fb23d59e29
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-06-22 14:46:11 +0800

    Update 线性筛积性函数.cpp

--- 数学/积性函数/线性筛积性函数.cpp
+++ 数学/积性函数/线性筛积性函数.cpp
@@ -1,6 +1,6 @@
 int mu[N];int phi[N];
 int np[N];vector<int> p;
-void Prework(int n = N - 10) {//线性筛积性函数
+void Prework(int n = N - 10) {
     np[0] = np[1] = 1;mu[1] = 1;phi[1] = 1;
     for (int i = 2;i <= n;i++) {
         if (!np[i]) {

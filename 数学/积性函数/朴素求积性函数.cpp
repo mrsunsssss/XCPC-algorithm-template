9f9c90f5aeecd2743b77da6f1114415188d97402
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-06-22 15:34:59 +0800

    Create 朴素求积性函数.cpp

---
+++ 数学/积性函数/朴素求积性函数.cpp
@@ -0,0 +1,11 @@
+int Phi(int n) {
+    int res = n;
+    for (int p = 2; p * p <= n; p++) {
+        if (n % p == 0) {
+            while (n % p == 0) n /= p;
+            res -= res / p;
+        }
+    }
+    if (n > 1) res -= res / n;
+    return res;
+}

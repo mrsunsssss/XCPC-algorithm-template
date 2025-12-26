24cb5aebc679041ee06e56efa68117924b6eb87d
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-12-26 19:29:28 +0800

    Update 线段树.cpp

--- 数据结构/线段树/lambda版本/线段树.cpp
+++ 数据结构/线段树/lambda版本/线段树.cpp
@@ -1,35 +1,37 @@
-    vector<int> tr(4 * n + 10), tag(4 * n + 10);
-    auto apply = [&](int p, int len, int v) {
-        tr[p] += len * v;
-        tag[p] += v;
+vector<int> tr(4 * n + 10), tag(4 * n + 10);
+    auto apply = [&](int u, int len, int v) {
+        tr[u] += len * v;
+        tag[u] += v;
         };
 
-    auto down = [&](int p, int l, int r) {
-        if (tag[p]) {
+    auto down = [&](int u, int l, int r) {
+        if (tag[u]) {
             int mid = (l + r) / 2;
-            apply(p * 2, mid - l + 1, tag[p]);
-            apply(p * 2 + 1, r - mid, tag[p]);
-            tag[p] = 0;
+            apply(u * 2, mid - l + 1, tag[u]);
+            apply(u * 2 + 1, r - mid, tag[u]);
+            tag[u] = 0;
         }
         };
 
-    auto upd = [&](auto&& upd, int p, int l, int r, int ql, int qr, int v) -> void {
+    auto upd = [&](auto&& upd, int u, int l, int r, int ql, int qr, int v) -> void {
         if (l > qr || r < ql) return;
         if (l >= ql && r <= qr) {
-            apply(p, r - l + 1, v);
+            apply(u, r - l + 1, v);
             return;
         }
-        down(p, l, r);
+        down(u, l, r);
+
         int mid = (l + r) / 2;
-        upd(upd, p * 2, l, mid, ql, qr, v);
-        upd(upd, p * 2 + 1, mid + 1, r, ql, qr, v);
-        tr[p] = tr[p * 2] + tr[p * 2 + 1];
+        upd(upd, u * 2, l, mid, ql, qr, v);
+        upd(upd, u * 2 + 1, mid + 1, r, ql, qr, v);
+
+        tr[u] = tr[u * 2] + tr[u * 2 + 1];
         };
 
-    auto ask = [&](auto&& ask, int p, int l, int r, int ql = 1, int qr) -> int {
+    auto ask = [&](auto&& ask, int u, int l, int r, int ql = 1, int qr) -> int {
         if (l > qr || r < ql) return 0;
-        if (l >= ql && r <= qr) return tr[p];
-        down(p, l, r);
+        if (l >= ql && r <= qr) return tr[u];
+        down(u, l, r);
         int mid = (l + r) / 2;
-        return ask(ask, p * 2, l, mid, ql, qr) + ask(ask, p * 2 + 1, mid + 1, r, ql, qr);
+        return ask(ask, u * 2, l, mid, ql, qr) + ask(ask, u * 2 + 1, mid + 1, r, ql, qr);
         };

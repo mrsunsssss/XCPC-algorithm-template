c58f7ecd9b4250544e5ff3dd5fdcd3a81fb5fcae
mrsunsssss <120580971+mrsunsssss@users.noreply.github.com>
2025-12-26 19:37:37 +0800

    Update 线段树.cpp

--- 数据结构/线段树/lambda版本/线段树.cpp
+++ 数据结构/线段树/lambda版本/线段树.cpp
@@ -1,4 +1,4 @@
-vector<int> tr(4 * n + 10), tag(4 * n + 10);
+    vector<int> tr(4 * n + 10), tag(4 * n + 10);
     auto apply = [&](int u, int len, int v) {
         tr[u] += len * v;
         tag[u] += v;
@@ -13,25 +13,27 @@ vector<int> tr(4 * n + 10), tag(4 * n + 10);
         }
         };
 
-    auto upd = [&](auto&& upd, int u, int l, int r, int ql, int qr, int v) -> void {
-        if (l > qr || r < ql) return;
-        if (l >= ql && r <= qr) {
+    auto _upd = [&](auto&& _upd, int u, int l, int r, int ql, int qr, int v) -> void {
+        if (l > qr or r < ql) return;
+        if (ql <= l and r <= qr) {
             apply(u, r - l + 1, v);
             return;
         }
         down(u, l, r);
 
         int mid = (l + r) / 2;
-        upd(upd, u * 2, l, mid, ql, qr, v);
-        upd(upd, u * 2 + 1, mid + 1, r, ql, qr, v);
+        _upd(_upd, u * 2, l, mid, ql, qr, v);
+        _upd(_upd, u * 2 + 1, mid + 1, r, ql, qr, v);
 
         tr[u] = tr[u * 2] + tr[u * 2 + 1];
         };
 
-    auto ask = [&](auto&& ask, int u, int l, int r, int ql = 1, int qr) -> int {
+    auto _ask = [&](auto&& _ask, int u, int l, int r, int ql, int qr) -> int {
         if (l > qr || r < ql) return 0;
         if (l >= ql && r <= qr) return tr[u];
         down(u, l, r);
         int mid = (l + r) / 2;
-        return ask(ask, u * 2, l, mid, ql, qr) + ask(ask, u * 2 + 1, mid + 1, r, ql, qr);
+        return _ask(_ask, u * 2, l, mid, ql, qr) + _ask(_ask, u * 2 + 1, mid + 1, r, ql, qr);
         };
+    auto upd = [&](int ql, int qr, int v) {return _upd(_upd, 1, 1, n, ql, qr, v);};
+    auto ask = [&](int ql, int qr) {return _ask(_ask, 1, 1, n, ql, qr);};
